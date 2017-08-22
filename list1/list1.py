a0f727194bfbefcbc55b9892fe6211050418729c
alanpaiva.asp <alanpaiva.asp@gmail.com>
2017-08-22 08:52:33 -0400

    Aula do massanori 22-08-17 _FATEC

--- list1/list1.py
+++ list1/list1.py
@@ -2,23 +2,24 @@
 
 ''' #Exercise 1 : 
 Faça um programa que peça dois números inteiros e imprima a soma desses dois números '''
-n = list()
-sum = 0
+numeros = list()
+soma = 0
+#Recebendo os valores e adicionando no array numeros[]
 for x in range (0,2):
-   #Recebendo o valor e adicionando no array n[]
    print("Digite o ",x+1,"numero:")
-   n.append(int(input()))
-   #somando o valor recebido no array n[]
-   sum += n[x]
-#printando o valor da soma depois de ter perguntado os 2 valores no for acima
-print("SOMA:",sum)
+   numeros.append(int(input()))
+   #somando o valor recebido no array numeros[]
+   soma += numeros[x]
+#printando o valor da soma depois de ter perguntado os 2 valores no FOR acima
+print("SOMA:",soma,"\n \n \n \n \n")
 #--------------------------------------------------------------------------------------------
 
 ''' #Exercise 2 : 
 Escreva um programa que leia um valor em metros e o exiba convertido em milímetros '''
-x = int(input("Digite o valor em metros: \n"))
-print(y*1000,"MM")
-
+#Recebendo o valor em metros
+metros = int(input("Digite o valor em metros: "))
+#Imprimindo em Milimetros(metros * 1000)
+print(metros*1000,"MM\n \n \n \n \n")
 
 
 #--------------------------------------------------------------------------------------------
@@ -27,28 +28,70 @@ print(y*1000,"MM")
 Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule
 o total em segundos. '''
 
+#Recebendo os valores de Dias, horas, minutos e segundos
+dias = int(input("Digite quantidade de dias: "))
+dias *= 86400
+horas = int(input("Digite quantidade em horas:"))
+horas *= 3600
+minutos = int(input("Digite quantidade em minutos:"))
+minutos *=60
+segundos = int(input("Digite quantidade em segundos:"))
+#somando o total em segundos
+segundos +=dias+horas+minutos
+#Imprimindo o valor
+print("Total em segundos:",segundos,"\n \n \n \n \n")
+
 #--------------------------------------------------------------------------------------------
 
 ''' #Exercise 4 : 
 Faça um programa que calcule o aumento de um salário. Ele deve solicitar o valor do salário e a
 porcentagem do aumento. Exiba o valor do aumento e do novo salário. '''
+#Recebendo os valores
+sal_atual = float(input("Digite seu salário atual: "))
+por_aumento = float(input("Digite porcentagem de aumento:"))
+#Calculando o aumento e o novo salário
+aumento = (sal_atual*aumento)/100
+novo_sal = aumento + sal_atual
+#printando na tela os resultados
+print("O valor do aumento será: ",aumento)
+print("O novo salário será: ",novo_sal, "\n \n \n \n \n")
+
 
 #--------------------------------------------------------------------------------------------
 
 ''' #Exercise 5 : 
 Solicite o preço de uma mercadoria e o percentual de desconto. Exiba o valor do desconto e o
 preço a pagar. '''
+#Recebendo os valores
+preco = float(input("Digite o valor da mercadoria: "))
+percent_desc = float(input("Digite o percentual de desconto: "))
+#Calculando os descontos
+desconto = (preco*percent_desc)/100
+novo_valor = desconto + preco
+# Imprimindo o resultado
+print("Valor do desconto: ",desconto,"R$")
+print("Novo valor: ",novo_valor,"R$ \n \n \n \n \n")
 
 #--------------------------------------------------------------------------------------------
 
 ''' #Exercise 6 : 
 Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média
 esperada para a viagem.  '''
+#Recebendo DeltaS e VM
+dist = float(input("Digite a distância da viagem em KM: "))
+vm = float(input("Digite a velocidade média esperada KM/H: "))
+#calculando com a formula de Velocidade média
+dt = vm/dist
+#imprimindo o resultado
+print("O tempo estimado de viagem é: ",dt,"horas \n \n \n \n \n")
 
 #--------------------------------------------------------------------------------------------
 
 ''' #Exercise 7 : 
 Converta uma temperatura digitada em Celsius para Fahrenheit. F = 9*C/5 + 32'''
+celsius = float(input("Digite a temperatura em Celsius"))
+fahren = ((9*celsius)/5)+32
+print("F°: ",fahren,"\n \n \n \n \n")
 
 #--------------------------------------------------------------------------------------------
 
