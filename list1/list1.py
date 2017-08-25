9cb30ec93e10126a76fba0b49a45755e3c519a3d
alanpaiva.asp <alanpaiva.asp@gmail.com>
2017-08-25 13:41:24 -0400

    comentando nos exercicios da lista 1

--- list1/list1.py
+++ list1/list1.py
@@ -89,16 +89,22 @@ print("O tempo estimado de viagem e: ",dt,"horas \n \n \n \n \n")
 
 ''' #Exercise 7 : 
 Converta uma temperatura digitada em Celsius para Fahrenheit. F = 9*C/5 + 32'''
+#Recebendo o valor em celsius
 celsius = float(input("Digite a temperatura em Celsius: "))
+#convertendo de celsius para fahrenheit com a equação mencionada acima
 fahren = ((9*celsius)/5)+32
+#imprimindo o resultado
 print("F : ",fahren,"\n \n \n \n \n")
 
 #--------------------------------------------------------------------------------------------
 
 ''' #Exercise 8 : 
 Faça agora o contrário, de Fahrenheit para Celsius. C/5 = F−32/9'''
+#Recebendo o valor em fahrenheit
 fahren = float(input("Digite a temperatura em Fahrenheit: "))
+#convertendo de fahrenheit para celsius com a equação mencionada acima
 celsius = ((fahren-32)/9) * 5
+#imprimindo o resultado
 print("Celsius C %.2f \n\n\n\n\n\n\n\n " %celsius)
 
 #--------------------------------------------------------------------------------------------
@@ -107,9 +113,10 @@ print("Celsius C %.2f \n\n\n\n\n\n\n\n " %celsius)
 Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo
 usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar,
 sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por km rodado.'''
-
+#recebendo os dados
 km = float(input("Quantidade de KM percorridos: "))
 dias = int(input("Quantidade de dias alugados: "))
+#imprimindo o resultado conforme a conta acima: 60 por dia x 15 cent. por km
 print("Total R$: %.2f \n\n\n\n\n\n\n"% ((dias*60)+(km*0.15)))
 
 #--------------------------------------------------------------------------------------------
@@ -119,9 +126,13 @@ Escreva um programa para calcular a redução do tempo de vida de um fumante. Pe
 quantidade de cigarros fumados por dia e quantos anos ele já fumou. Considere que um fumante
 perde 10 minutos de vida a cada cigarro, calcule quantos dias de vida um fumante perderá. Exiba o
 total de dias.'''
+#recebendo os valores
 cig = int(input("Favor, digite quantos cigarros fumados por dia: "))
 anos = int(input("Favor, digite quantos anos ja fumou:"))
-print("Voce perdera %d dias de sua vida \n\n\n\n\n\n\n\n" %((cig*10)*(anos*365)/24))
+#calculando quantos dias de vida perderá cigarros fumados por dia x 10 (pra ter o valor em minutos por dia),
+#anos x 365 (para ter o valor de anos em dias), multiplica o valor de minutos por dia x total de dias (para ter o valor total em minutos)
+# e divide por  1440 para converter o valor de minutos para dias, pois 1440 minutos = 1dia
+print("Voce perdera %d dias de sua vida \n\n\n\n\n\n\n\n" %((cig*10)*(anos*365)/1440))
 
 
 #--------------------------------------------------------------------------------------------
@@ -129,6 +140,7 @@ print("Voce perdera %d dias de sua vida \n\n\n\n\n\n\n\n" %((cig*10)*(anos*365)/
 ''' #Exercise 11 : 
 Sabendo que str( ) converte valores numéricos para string, calcule quantos dígitos há em 2 elevado
 a um milhão. '''
+#imprimindo o solicitado convertendo int para str e pegando a quantidade de algarismos através do len
 print ("Existem %s digitos em 2^1000000" %len(str(2**1000000)))
 
 
