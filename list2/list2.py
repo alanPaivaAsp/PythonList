da2fca7614cbff46eb3cd207b8b737c5948942c7
alanpaiva.asp <alanpaiva.asp@gmail.com>
2017-09-01 10:21:13 -0400

    allExercises_list2_done

--- list2/list2.py
+++ list2/list2.py
@@ -58,15 +58,45 @@ if num[0] > num[1] and num[0] > num[2]:
   maior = num[0]
 elif num[1] > num[0] and num[1] > num[2]:
   maior = num[1]
-else:
+elif num[2] > num[0] and num[2] > num[1]:
   maior = num[2]
-print('Maior numero : %d \n\n\n\n\n\n\n\n\n\n\n' %maior)
+else:
+  maior = 'Todos ou dois numeros iguais'
+print('Maior numero : ' , maior, '\n\n\n\n\n\n\n\n\n\n\n\n')
 
   
 #__________________________________________________________________________________________________________
 '''
 EXERCISE 5
 Faça um Programa que leia três números e mostre o maior e o menor deles.'''
+maior = 0
+menor = 0
+num = list ()
+for x in range (0,3):
+  print('Digite %d numero: ' %(x+1))
+  num.append(int(input()))
+  
+if num[0] > num[1] and num[0] > num[2]:
+  maior = num[0]
+elif num[1] > num[0] and num[1] > num[2]:
+  maior = num[1]
+elif num[2] > num[0] and num[2] > num[1]:
+  maior = num[2]
+else:
+  maior = 'Todos ou dois numeros iguais'
+  
+
+if num[0] < num[1] and num[0] < num[2]:
+  menor = num[0]
+elif num[1] < num[0] and num[1] < num[2]:
+  menor= num[1]
+elif num[2] < num[0] and num[2] < num[1]:
+  menor = num[2]
+else:
+  menor = 'Todos ou dois numeros iguaiss'
+ 
+print('Maior numero : ' , maior)
+print('Menor numero : ' , menor, '\n\n\n\n\n\n\n\n\n\n\n\n')
 #__________________________________________________________________________________________________________
 ''' 
 EXERCISE 6
@@ -77,9 +107,22 @@ quanto pagou ao sindicato e o salário líquido. Observe que Salário Bruto - De
 Calcule os descontos e o salário líquido, conforme a tabela abaixo:
 a. + Salário Bruto : R$
 b. - IR (11%) : R$
-c. - INSS (8%) : R$
+c. - INSS (8%) : R$%
 d. - Sindicato ( 5%) : R$
 e. = Salário Liquido : R$'''
+ganho_hora = int(input('Digite o quanto voce ganha por hora: '))
+horas_trab = int(input('Digite horas trabalhadas: '))
+salario = ganho_hora*horas_trab
+imposto = salario*0.11
+inss = salario*0.08
+sindicato = salario*0.05
+print('salario Bruto: Rs%.2f' %(salario))
+print('IR (11 por cento) : Rs%.2f' %imposto)
+print('INSS (8 por cento) : Rs%.2f'%inss)
+print('Sindicato (5 por cento) : Rs%.2f'%sindicato)
+print ('Salario Liquido : Rs%.2f \n\n\n\n\n\n\n\n\n\n'%(salario-(inss+imposto+sindicato)))
+
+
 #__________________________________________________________________________________________________________
 
 ''' 
@@ -88,3 +131,15 @@ Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em
 Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros,
 que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total. 
 Obs. : somente são vendidos um número inteiro de latas.'''
+import math
+metros_quadrad = float(input('Digite o tamanho em metros quadrados: '))
+litros =metros_quadrad/3
+latas = 1
+preco = 80.00
+if litros > 18:
+  latas = int(math.ceil(litros/18))
+preco = float(latas*80)
+print ('Serao necessario %d latas' %latas)
+print ('Preco total RS%.2f \n\n\n\n\n\n\n\n\n' %preco)
+
+
